f2357efb4c707a77972ae2872cada6d61a8f9c3f
Anatoliy Shishkin <anatoliy.shishkin@wikimart.ru>
2018-06-12 19:45:59 +0300

    cryptogmes

---
+++ client/lang/en/faq.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  q1: 'How to buy a ticket',
+  a1: '1) Buy ticket using Metamask browser extension 2) Send ETH amount equal ticket price to game smart contract address',
+
+  q2: 'When round finished',
+  a2: 'Round finishes when the last ticket in the current round has sold',
+
+  q3: 'How to get my Gain',
+  a3: 'Your gain will sent automatically to address which bought ticket',
+
+  q4: 'If i will transfer ETH amount not equal ticket price, what will happen',
+  a4: 'Transaction will be rejected',
+
+
+}
